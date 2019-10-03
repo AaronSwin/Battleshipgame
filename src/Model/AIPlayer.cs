bd3c4aff04e27364216644e32266be5916013230
benmcconnonswin <54128138+benmcconnonswin@users.noreply.github.com>
2019-10-03 12:58:06 +1000

    Fixed crash when AI sinks ship

--- src/Model/AIPlayer.cs
+++ src/Model/AIPlayer.cs
@@ -54,10 +54,6 @@ public abstract class AIPlayer : Player
         /// <param name="this">location 1</param>
         /// <param name="other">location 2</param>
         /// <returns>true if location 1 and location 2 are at the same spot</returns>
-        public static bool operator ==(Location @this, Location other)
-        {
-            return @this != null && other != null && @this.Row == other.Row && @this.Column == other.Column;
-        }
 
         /// <summary>
         /// Check if two locations are not equal
@@ -65,10 +61,6 @@ public abstract class AIPlayer : Player
         /// <param name="this">location 1</param>
         /// <param name="other">location 2</param>
         /// <returns>true if location 1 and location 2 are not at the same spot</returns>
-        public static bool operator !=(Location @this, Location other)
-        {
-            return @this == null || other == null || @this.Row != other.Row || @this.Column != other.Column;
-        }
     }
 
 
